e6720d286bdc0376de107c38990d48ebff8aae6a
nayaib98 <nayaiburgess@gmail.com>
2019-01-10 13:19:22 -0500

    1st attempt to show controller, added ERD to file, created more view files

--- controllers/showController.js
+++ controllers/showController.js
@@ -3,7 +3,47 @@ const shows = require('../models/show')
 const showController = {
     index: (req, res) => {
         shows.find({}).then((shows) => {
-            res.send(shows)
+            res.render('views/show', {shows})
+        })
+    },
+    new: (req, res) => {
+        res.render("views/newshow")
+    },
+    create: (req, res) => {
+        console.log(req.body)
+        shows.create({
+            name: req.body.name,
+            date: req.body.date,
+            address: req.body.address,
+            email: req.body.email,
+            flyer: req.body.flyer
+        }).then(shows => {
+            res.redirect('/fashionshows')
+        })
+    },
+    show: (req, res) => {
+        const showId = req.params.id
+        shows.findById(showId).then((show) => {
+          console.log(show)
+          res.render('views/show', { show })
+        })
+      },
+    edit: (req, res) => {
+        const showId = req.params.id
+        // console.log(newslinkId)
+        res.render('view/editshow', {showId})
+    },
+    update: (req, res) => {
+        const showId = req.params.id
+        console.log(req.body)
+        showos.findByIdAndUpdate(showId, req.body, {new: true}).then((show) => {
+            res.redirect(`/fashionshows/:id`)
+        })
+    },
+    delete: (req, res) => {
+        const showId = req.params.id
+        shows.findByIdAndRemove(showId).then(() => {
+            res.redirect('/modelprofile/:id')
         })
     }
 }
